9731dba5bc492a7eebaef751493447f983f22317
Cybernetic1 <generic.intelligence@gmail.com>
2020-06-12 15:06:44 +0800

    init

---
+++ ttt-test.py
@@ -0,0 +1,35 @@
+import gym
+import gym_tictactoe
+env = gym.make('TicTacToe-v1', symbols=[-1, 1], board_size=3, win_size=3)
+
+user = 0
+done = False
+reward = 0
+
+# Reset the env before playing
+state = env.reset()
+
+while not done:
+	env.render(mode=None)
+	if user == 0:
+		state, reward, done, infos = env.step(env.action_space.sample(), -1)
+	elif user == 1:
+		state, reward, done, infos = env.step(env.action_space.sample(), 1)
+
+	# If the game isn't over, change the current player
+	if not done:
+		user = 0 if user == 1 else 1
+	else :
+		if reward == 10:
+			print("Draw !")
+		elif reward == -20:
+			print("Infos : " + str(infos))
+			if user == 0:
+				print("Random wins ! AI Reward : " + str(reward))
+			elif user == 1:
+				print("AI wins ! AI Reward : " + str(-reward))
+		elif reward == 20:
+			if user == 0:
+				print("AI wins ! AI Reward : " + str(reward))
+			elif user == 1:
+				print("Random wins ! AI Reward : " + str(reward))
